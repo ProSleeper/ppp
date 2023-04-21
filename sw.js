a600f3693433c90dce8f5f49d4b99e6ad10fe9fc
ASUD <ingngnin@gmail.com>
2023-04-21 19:11:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -52,3 +52,11 @@ self.addEventListener("activate", function (e) {
         })
     );
 });
+
+Notification.requestPermission().then((status) => {
+    console.log("Notification 상태", status);
+
+    if (status === "denied") {
+        alert("Notification 거부됨");
+    }
+});
