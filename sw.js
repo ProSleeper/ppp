3563b0ccf9f91c556f8ce4e7d2fbf387f3e8b546
ASUD <ingngnin@gmail.com>
2023-04-26 17:37:49 +0900

    update

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
-]
+var URLS = [
+    // Add URL you want to cache in this list.
+    "/ppp/", // If you have separate JS/CSS files,
+    "/ppp/index.html", // add path to those files here
+];
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
