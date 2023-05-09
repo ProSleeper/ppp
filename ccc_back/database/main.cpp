ce47c3e2e9f7f0798d9e7aa2c726b675422e6918
prosleeper <ingngnin@gmail.com>
2023-05-09 17:47:06 +0900

    test: c++ js database input test for direct query input

--- ccc_back/database/main.cpp
+++ ccc_back/database/main.cpp
@@ -1,11 +1,24 @@
-#include "mysql_connector.h"
+#include <iostream>
 #include <nlohmann/json.hpp>
+
+#include "mysql_connector.h"
+
+using json = nlohmann::json;
+
 typedef struct {
     string brand;
     string url;
 } ProductUrl;
 
+void ltrim(std::string &str) {
+    str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](unsigned char ch) { return !std::isspace(ch); }));
+}
+
 int main(int argc, char *argv[]) {
+    json json_object = {{"key", "value"}};
+    printf("%s\n", json_object.dump().c_str());
+    return 0;
+
     const string strServer = "localhost";  // where the mysql database is
     const string strUser = "u0_a177";      // the root user of mysql
     const string strPassword = "suzi123";  // the password of the root user in mysql
@@ -15,11 +28,19 @@ int main(int argc, char *argv[]) {
 
     MysqlConnector *mCon = new MysqlConnector(strServer, strUser, strPassword, strDatabase);
     string strQuery;
-    string strSelectDataAll = "select * from product_url";
-    string strInsertData = "insert into product_url(brand, url) values('" + strArgv1 + "', '" + strArgv2 + "')";
+    // string strSelectDataAll = "select * from product_url";
+    // string strInsertData = "insert into product_url(brand, url) values('" + strArgv1 + "', '" + strArgv2 + "')";
+
+    while (true) {
+        cin >> strQuery;
+        ltrim(strQuery);
+        transform(strQuery.begin(), strQuery.end(), strQuery.begin(), ::tolower);
+        strQuery.find("insert");
+        mCon->Select(strQuery);
+    }
 
     // mCon->Insert(strInsertData);
-    mCon->Select(strSelectDataAll);
+    // mCon->Select(strSelectDataAll);
 
     // mCon->SendQuery(cQuery);
     // const std::vector<ResultSet *> *rs = mCon->GetResultSet();
--- ccc_back/database/test.cpp
+++ ccc_back/database/test.cpp
@@ -1,16 +1,30 @@
-#include <cstdio>
+#include <algorithm>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main(int argc, const char** argv) {
-    string aaa = "123";
-    string bbb = aaa;
+void ltrim(std::string& str) {
+    str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](unsigned char ch) { return !std::isspace(ch); }));
+}
 
-    aaa = "ccc0";
-    cout << aaa << endl;
-    cout << bbb << endl;
+bool checkQuery(string& str) {
+    ltrim(str);
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    std::string sub = str.substr(0, 6);
+    size_t pos = sub.find("insert");
+    if (pos != std::string::npos) {
+        // 검색어가 발견된 경우의 처리
+        // ...
+        cout << "insert found" << endl;
+    }
+}
+
+int main(int argc, const char** argv) {
+    string strSelectDataAll = "select * from product_url";
+    string strInsertData = "INSERT INTO PRODUCT_URL(BRAND, URL) VALUES('')";
+    checkQuery(strInsertData);
+    // std::string str = "insert this string";
 
     return 0;
 }
