bacb3af69129c9662f86bc6ddb12ec0eb9fc8277
ASUD <ingngnin@gmail.com>
2023-05-19 18:09:35 +0900

    test: 각각의 레포지토리 만드는 중. 좀 많은 수정이나 추가가 필요할듯.

--- ccc/src/working/entity/monthly_data.js
+++ ccc/src/working/entity/monthly_data.js
@@ -8,6 +8,9 @@ class monthly_data {
             url: url,
             month: today,
         };
+        for (let index = 0; index < 31; index++) {
+            this.data[`day${index}_price`] = null;
+        }
         this.data[`day${Parse_Day(today)}_price`] = price;
         this.data[`primary_key`] = this.data.url;
     }
