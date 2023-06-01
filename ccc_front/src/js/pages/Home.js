a35e85a9b2aa286aad6f31e3d79dbba45d7db31c
kyw <ingngnin@gmail.com>
2023-06-01 22:29:54 +0900

    test: push pwa

--- ccc_front/src/js/pages/Home.js
+++ ccc_front/src/js/pages/Home.js
@@ -3,14 +3,46 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
+
+
+
 const Home = () => {
+    const use_url = process.env.REACT_APP_API_URL;
+
+    const store_subscriber_url = use_url + "/store_push_sub_data";
+
+    const store_sub = (reqUrl, subscriber) => {
+        fetch(reqUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(subscriber),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.text();
+                }
+                throw new Error("Network response was not ok");
+            })
+            .then((result) => {
+                if (result) {
+                    console.log("store_subscriber_ok");
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
     const notiFunc = () => {
         navigator.serviceWorker.ready.then((registration) => {
             registration.pushManager.getSubscription().then((subscription) => {
                 if (subscription) {
                     //save subscription on DB
-                    console.log("old");
-                    console.log(subscription);
+                    // console.log("old");
+                    // console.log(subscription);
+                    store_sub(store_subscriber_url, subscription);
                 } else {
                     registration.pushManager
                         .subscribe({
@@ -19,10 +51,10 @@ const Home = () => {
                                 "BKA9rsMuwAfknd92kUS5-PbL2A7P-viDmPh3WxtlrDo-tGH4D-E1H4jrJJRkCm0UBdiDN7Ikw5K35JoaSYVFcAA",
                         })
                         .then((subscription) => {
+                            store_sub(store_subscriber_url, subscription);
                             //save subscription on DB
-                            console.log("new");
-                            console.log(subscription);
-
+                            // console.log("new");
+                            // console.log(subscription);
                         });
                 }
             });
