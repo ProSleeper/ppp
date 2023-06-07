b1033ab80c4ef2ff3723daf090d18cbe7757ed56
kyw <ingngnin@gmail.com>
2023-06-08 04:46:43 +0900

    test: push 원복

--- ccc_back/src/main/router/push.js
+++ ccc_back/src/main/router/push.js
@@ -4,12 +4,12 @@ module.exports = (app) => {
     app.post("/store_push_sub_data", async (req, res) => {
         console.log(req.body);
 
-        const user_cookie = req.body.cookie;
+        // const user_cookie = req.body.cookie;
         const user_endpoint = req.body.endpoint;
         const user_p256dh = req.body.keys.p256dh;
         const user_auth = req.body.keys.auth;
 
-        console.log(user_cookie);
+        // console.log(user_cookie);
         console.log(user_endpoint);
         console.log(user_p256dh);
         console.log(user_auth);
