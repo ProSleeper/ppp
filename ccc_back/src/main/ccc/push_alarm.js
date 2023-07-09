ef86255a94b93b64c7aa1db48bdda055183a2586
ProSleeper <ingngnin@gmail.com>
2023-07-10 02:34:43 +0900

    Update push_alarm.js

--- ccc_back/src/main/ccc/push_alarm.js
+++ ccc_back/src/main/ccc/push_alarm.js
@@ -34,7 +34,7 @@ const push_alarm = async (product_sale_list, total_subscriber_list) => {
     for (const subscriber_data of total_subscriber_list) {
         for (const sale_data of product_sale_list) {
             try {
-                console.log("sale_data.title");
+                console.log(sale_data.title);
                 await webpush.sendNotification(
                     {
                         endpoint: subscriber_data.endpoint,
