60f4adc0a788d258af4bc48c5d4609de587cde2b
kyw <ingngnin@gmail.com>
2023-10-09 02:30:14 +0900

    test: ssl 인증은 완료! 이제 다시 http 요청이 제대로 오는지와 http요청으로 오면 https로 리다이렉트 되는지 확인

--- ccc_back/main.js
+++ ccc_back/main.js
@@ -10,28 +10,28 @@ const was = require("./src/main/router/was");
 const api = require("./src/main/router/api");
 const push = require("./src/main/router/push");
 
-// const options = {
-//     key: fs.readFileSync(path.join(__dirname, "/ssl_keys/private.key"), "utf8"),
-//     cert: fs.readFileSync(path.join(__dirname, "/ssl_keys/certificate.crt"), "utf8"),
-//     ca: fs.readFileSync(path.join(__dirname, "/ssl_keys/ca_bundle.crt"), "utf8"),
-// };
+const options = {
+    key: fs.readFileSync(path.join(__dirname, "/ssl_keys/private.key"), "utf8"),
+    cert: fs.readFileSync(path.join(__dirname, "/ssl_keys/certificate.crt"), "utf8"),
+    ca: fs.readFileSync(path.join(__dirname, "/ssl_keys/ca_bundle.crt"), "utf8"),
+};
 
 const portForHttp = 4000;
-// const portForHttps = 4001;
+const portForHttps = 4001;
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-//     const redirect_https_url = process.env.HTTPS_URL + req.url;
-//     if (!req.secure) {
-//         res.redirect(redirect_https_url);
-//     } else {
-//         next();
-//     }
-// });
+app.use((req, res, next) => {
+    const redirect_https_url = process.env.HTTPS_URL + req.url;
+    if (!req.secure) {
+        res.redirect(redirect_https_url);
+    } else {
+        next();
+    }
+});
 
 //이 부분에서 was, api, push 다 추가하면 될듯.
 was(app, express);
@@ -39,12 +39,12 @@ api(app);
 push(app);
 
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(options, app);
+const httpsServer = https.createServer(options, app);
 
 httpServer.listen(portForHttp, function () {
     console.log("Http server listening on port " + portForHttp);
 });
 
-// httpsServer.listen(portForHttps, function () {
-//     console.log("Https server listening on port " + portForHttps);
-// });
+httpsServer.listen(portForHttps, function () {
+    console.log("Https server listening on port " + portForHttps);
+});
